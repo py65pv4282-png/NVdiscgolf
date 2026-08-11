000e8a299df1b129c7d7447e74b6b931b9d9ecd7
py65pv4282-png <py65pv4282@privaterelay.appleid.com>
2026-08-11 21:54:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,18 @@
-const CACHE = "nvdiscgolf-v2";
+const CACHE = "nvdiscgolf-v3";
+
 const FILES = [
   "./",
   "./index.html",
   "./styles.css",
   "./app.js",
   "./manifest.webmanifest",
-  "./assets/hal-1-karta.jpg",
-  "./assets/hal-2-karta.jpg",
+  "./Hål1.jpg",
+  "./Hål2.jpg",
+  "./Hål3.heic",
+  "./Hål4.jpg",
+  "./Hål5.jpg",
+  "./Hål6.jpg",
+  "./Hål7.jpg",
   "./assets/icon-192.png",
   "./assets/icon-512.png"
 ];
@@ -18,17 +24,29 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE).then(cache => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match("./index.html")))
+    caches.match(event.request).then(
+      cached =>
+        cached ||
+        fetch(event.request)
+          .then(response => {
+            const copy = response.clone();
+            caches.open(CACHE).then(cache => cache.put(event.request, copy));
+            return response;
+          })
+          .catch(() => caches.match("./index.html"))
+    )
   );
 });
